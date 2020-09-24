1defa3294735d8d970a77574dd3c4acb56bdbef1
Anna <azobnina97@mail.ru>
2020-09-24 11:14:54 +0300

    Animation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-  var uncheck = document.getElementsByTagName('input');
-  for(var i = 0; i < uncheck.length; i++) {
-      if(uncheck[i].type == 'checkbox') {
-        uncheck[i].checked = false;
-      }
-  }
+    var uncheck = document.getElementsByTagName('input');
+    for(var i = 0; i < uncheck.length; i++) {
+        if(uncheck[i].type == 'checkbox') {
+          uncheck[i].checked = false;
+        }
+    }
 
 
     var last_checkbox, pre_last_checkbox = '';
     $('.faq__item__input').change(function() {
+        
+
         var pdi = $(this).parent('.faq__item').index();
         if(pre_last_checkbox === ''){
           last_checkbox = pre_last_checkbox = pdi;
@@ -20,7 +22,29 @@ $(document).ready(function() {
         if($('.faq__item__input:checked').length > 1){
           $('.faq__item').eq(pre_last_checkbox).find('.faq__item__input').prop('checked', false);
         }
+
+
+        // if($(this).prop('checked') == true) {
+        //   $(this).parent().siblings('.faq__item__text').css('max-height', '500px');
+
+        //   $(this).siblings('.faq__item__icon').css('background', 'linear-gradient(180deg, #E41579 0%, #FF3F84 100%)');
+        //   $(this).siblings('.faq__item__icon').children('.faq__item__icon__symbol').css('background-color', '#fff');
+
+        //   $(this).siblings('.faq__item__icon').children('.faq__item__icon__symbol').css('transform', 'rotate(45deg) translate3d(-200%, -50%, 0)');
+        // }
+       
+          // $(this).parent().siblings('.faq__item__text').css('max-height', '500px');
+
+          // $(this).siblings('.faq__item__icon').css('background', 'linear-gradient(180deg, #E41579 0%, #FF3F84 100%)');
+          // $(this).siblings('.faq__item__icon').children('.faq__item__icon__symbol').css('background-color', '#fff');
+
+          // $(this).siblings('.faq__item__icon').children('.faq__item__icon__symbol').css('transform', 'rotate(45deg) translate3d(-200%, -50%, 0)');
     });
+
+    // $('.faq__item__input').change(function() {
+    //     $(this).parent().siblings('.faq__item__text').css('max-height', '500px');
+    //   }
+    // );
 });  
 
 
